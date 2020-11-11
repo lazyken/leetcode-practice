4036eba4b803113f36585377dfd7954f84c0dfdf
zhangxiaoyu <wykenken@163.com>
2020-11-12 00:15:04 +0800

    feat: update docs

--- 二分查找.js
+++ 二分查找.js
@@ -2,7 +2,12 @@
 function binarySearch(arr, target) {
   var l = 0;
   var r = arr.length - 1;
+  // 在[l...r]的范围里寻找target
   while (l <= r) {
+    // 当l=== r时，区间[l...r]依然是有效的
+
+    // var mid = Math.ceil((l + r) / 2);
+    // 当l和r足够大时，l+r的值可能会整型溢出，可以使用减法
     var mid = l + Math.ceil((r - l) / 2);
     if (arr[mid] === target) {
       return mid;
